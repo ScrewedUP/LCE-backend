3726f8e274b70320e1997a7215f5be7ed4002866
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-09-17 18:36:39 +0530

    Iniital commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
